1c0a0448b97cb93d86b3000084b07b2702c0d8da
ViChanzo <75827403+vichanzo@users.noreply.github.com>
2024-05-27 22:28:22 -0700

    Create 1-1-winget.ps1

---
+++ 1-1-winget.ps1
@@ -0,0 +1,13 @@
+winget install WinSCP.WinSCP -e -h --accept-source-agreements --accept-package-agreements 
+
+winget install -e -h --id Notepad++.Notepad++
+winget install -e -h --id WinMerge.WinMerge
+winget install -e -h --id WinSCP.WinSCP
+winget install -e -h --id mRemoteNG.mRemoteNG
+winget install -e -h --id 7zip.7zip
+winget install -e -h --id Microsoft.VisualStudioCode			# This installs MS VScode by default, comment this out if you want VScodium
+# winget install -e -h --id VSCodium.VSCodium
+winget install -e -h --id Microsoft.WindowsTerminal			# Windows Terminal
+winget install -e -h --id Mozilla.Firefox
+# Keepass2 - manually download and install since there is no way to install this via winget.
+winget upgrade --all --accept-source-agreements --accept-package-agreements 
