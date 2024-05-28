1848c6bbc09eb2b4fe1425d7d21b1f83c3e4e699
vichanzo <mngr@DESKTOP-QN3T7S8>
2024-05-28 00:09:39 -0700

    added winget git

--- 1_1-winget.ps1
+++ 1_1-winget.ps1
@@ -5,6 +5,7 @@ winget install -e -h --id WinMerge.WinMerge
 winget install -e -h --id WinSCP.WinSCP
 winget install -e -h --id mRemoteNG.mRemoteNG
 winget install -e -h --id 7zip.7zip
+winget install -e -h --id git.git
 winget install -e -h --id Microsoft.VisualStudioCode			# This installs MS VScode by default, comment this out if you want VScodium
 # winget install -e -h --id VSCodium.VSCodium
 winget install -e -h --id Microsoft.WindowsTerminal			# Windows Terminal
